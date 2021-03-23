53997299218b6979828027f913ffd5bb84f7949d
vilara ubuntu fujtsu <marcelovilara@gmail.com>
2021-03-23 16:21:42 -0300

    datatables parte 2

--- app/Http/Livewire/Model/User/Lista.php
+++ app/Http/Livewire/Model/User/Lista.php
@@ -5,6 +5,7 @@ namespace App\Http\Livewire\Model\User;
 use Livewire\Component;
 use Livewire\WithPagination;
 use App\Models\User;
+use Illuminate\Database\Eloquent\Collection;
 
 class Lista extends Component
 {
@@ -12,21 +13,44 @@ class Lista extends Component
     use WithPagination;
 
     public $headers;
+    public $sortColumn = 'created_at';
+    public $sortDirection = 'asc';
+    public $searchTerm;
 
-    private function headerConfig(){
+    public function headerConfig()
+    {
         return [
-            'id' => '#',
+            'id' => 'id',
             'name' => 'Login',
             'email' => 'Email',
+            'created_at' => 'Criado'
         ];
     }
 
-    public function mount(){
+    public function mount()
+    {
         $this->headers = $this->headerConfig();
+        // $this->nome = $nome;
     }
 
-    public function resultData(){
-        return User::all();
+
+
+    public function sort($column)
+    {
+        $this->sortColumn = $column;
+        $this->sortDirection = $this->sortDirection == 'asc' ? 'desc' : 'asc';
+    }
+
+    private function resultData()
+    {
+        return User::search('Marcelo')->paginate(10);
+        /* return User::where(function ($query) {
+            $query->where('name', '!=', 't');
+            if ($this->searchTerm != "") {
+                $query->where('name', 'like', '%' . $this->searchTerm . '%');
+                $query->orWhere('email', 'like', '%' . $this->searchTerm . '%');
+            }
+        })->orderBy($this->sortColumn, $this->sortDirection)->paginate(15); */
     }
 
     public function render()
--- resources/views/livewire/model/user/lista.blade.php
+++ resources/views/livewire/model/user/lista.blade.php
@@ -1,21 +1,47 @@
-<div>
-    <div class="flex justify-center items-center h-20 bg-green-100">
+@php
+ //  dd($data);
+@endphp
+<div class="overflow-x-auto">
+    <div
+        class="min-w-screen min-h-screen bg-gray-100 flex items-start justify-center bg-gray-100 font-sans overflow-hidden">
+        <div class="w-full lg:w-5/6">
+            <div class="bg-white shadow-md rounded">
+                <x-jet-input id="civil.primeiro_nome" type="text" class="mb-3"   wire:model="searchTerm" placeholder="Procurar..."/>
+                <table class="min-w-max w-full table-auto bg-gray-200">
+                    <thead>
+                        <tr class="bg-gray-300 text-gray-600 uppercase text-sm leading-normal">
 
-        <table class="table table-striped">
-            <thead>
-                @foreach ($headers as $key => $value)
-                    <th>{{ $value }}</th>
-                @endforeach
+                            @foreach ($headers as $key => $value)
 
-            </thead>
-            <tbody>
-                <tr>
-                    <td>rere</td>
-                    <td>rere</td>
-                    <td>rere</td>
-                </tr>
-            </tbody>
-
-        </table>
+                                <th class="py-3 px-6 text-left cursor-pointer" wire:click="sort('{{ $key }}')">
+                                    @if ($sortColumn == $key)
+                                        <span>{!! $sortDirection == 'asc' ? '&#8659' : '&#8657;' !!}</span>
+                                    @endif
+                                    {{ $value  }}
+                                </th>
+                            @endforeach
+                        </tr>
+                    </thead>
+                    <tbody class="text-gray-600 text-sm font-light">
+                        @if (count($data))
+                            @foreach ($data as $item)
+                                <tr class="border-b border-gray-300 hover:bg-gray-100">
+                                    @foreach ($headers as $key => $value)
+                                        <td class="py-3 px-6 text-left whitespace-nowrap">
+                                            {{ $key == 'created_at' ? $item->$key->diffForHumans() : $item->$key }}
+                                        </td>
+                                    @endforeach
+                                </tr>
+                            @endforeach
+                            @else
+                            <tr class="border-b border-gray-300 hover:bg-gray-100">
+                                <td class="py-3 px-6 text-left whitespace-nowrap">Sem resultados
+                                </td></tr>
+                        @endif
+                    </tbody>
+                </table>
+                {{ $data->links() }}
+            </div>
+        </div>
     </div>
 </div>
